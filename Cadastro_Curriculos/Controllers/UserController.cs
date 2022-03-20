feccdf8ff0248b4ce236777da29f6207148254f7
Alexandrade01 <solidwinter96@outlook.com>
2022-03-19 22:06:59 -0300

    Criacao projeto

---
+++ Cadastro_Curriculos/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Cadastro_Curriculos.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadastro_Curriculos.Controllers
+{
+    public class UserController:Controller
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
